fcf969bbdbd765a93cdb5bc786691a03ca8ce8e2
tess hsu <tess.hsu@gmail.com>
2025-10-13 15:06:04 +0200

    fix-port

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello from FastAPI on Minikube 🚀"}
+
+@app.get("/ready")
+async def readiness():
+    return {"status": "ok"}
+
+@app.get("/health")
+async def health():
+    return {"status": "ok"}
+
